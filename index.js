1e73d7e4a5513a185f5d5cef91cdd475bf068275
Smalllong <m18818276304@icloud.com>
2022-05-04 19:40:56 +0800

    optimize

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
-// Require the framework and instantiate it
-var fs = require('fs')
-
 const fastify = require('fastify')({
-  logger: true,
-})
+    logger: true,
+  }),
+  fs = require('fs')
 
 fastify.register(require('@fastify/static'), {
   root: require('path').join(__dirname, 'public'),
 })
 
-// Declare a route
 fastify.get('/', function (request, reply) {
   reply.sendFile('index.html', __dirname)
 })
 fastify.get('/ls', function (request, reply) {
-  var files = fs.readdirSync('./public/' + request.query.path, { withFileTypes: true }).map((f) => ({
-    name: f.name,
-    dir: f.isDirectory(),
-  }))
-  reply.send({ files: files })
+  reply.send({
+    files: fs.readdirSync('./public/' + request.query.path, { withFileTypes: true }).map((f) => ({
+      name: f.name,
+      dir: f.isDirectory(),
+    })),
+  })
 })
 
-// Run the server!
 fastify.listen(3000, '::', function (err, address) {
   if (err) {
     fastify.log.error(err)
